2d38c71e9dd3a3bd5ff13672663f6b1cd3c29a7b
ChrismalyEscarfuller <chrismalyescarfuller@gmail.com>
2024-05-24 12:35:04 -0400

    Add files via upload

--- tareadellibro/tarea001/main.c
+++ tareadellibro/tarea001/main.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void main(void)
+void main(void)//Esta línea declara la función principal del programa.
 {
-    float PRO;
-    printf("ingrese el promedio del alumno: ");
-    scanf("%f", &PRO);
-    if (PRO >= 6)
-        printf ("\nAprobado");
-}
+    float PRO;//Declarar una variable de tipo float llamada "PRO" para almacenar el promedio del alumno.
+    printf("ingrese el promedio del alumno: ");//Imprimir un mensaje solicitando al usuario que ingrese el promedio del alumno.
+    scanf("%f", &PRO);//Leer el valor ingresado por el usuario y almacenarlo en la variable "PRO".
+    if (PRO >= 6)//Verificar si el promedio del alumno es mayor o igual a 6.
+        printf ("\nAprobado");//Si el promedio es mayor o igual a 6, imprimir el mensaje "Aprobado".
+}//Esta línea cierra la instrucción condicional if.
