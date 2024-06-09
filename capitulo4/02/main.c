999cf7d6fa53748ec5e2cc56c46d5da057c60a97
ChrismalyEscarfuller <chrismalyescarfuller@gmail.com>
2024-06-09 19:56:27 -0400

    Add files via upload

---
+++ capitulo4/02/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int cubo(void);
+int i;
+
+void main(void)
+{
+    int cub;
+    for (i=1; i <= 10; i++)
+    {
+        cub = cubo();
+        printf("\nEl cubo de %d es:  %d", i, cub);
+    }
+}
+int cubo(void)
+
+{
+    int i = 2;
+    return (i*i*i);
+}
